47591fd63dc65b8e427da7f19d1f968772d9d7cc
Lars Holm Nielsen <lars.holm.nielsen@cern.ch>
2020-11-03 15:53:17 +0100

    Adds command to list PRs

--- github.py
+++ github.py
@@ -15,31 +15,56 @@ GLOBAL_PACKAGES = [
 ]
 
 class MarkdownFormatter:
-    def header(self):
+    def commit_header(self):
         print('| Package name | Version | Unreleased commits |')
         print('|----|----|----|')
 
 
-    def body_item(self, res):
+    def commit_body(self, res):
         print(f'| {res.name} | {res.pypi_version} | |')
 
         for _dummy, headline in res:
             print(f'| | | {headline} |')
 
-    def footer(self):
+    def commit_footer(self):
+        pass
+
+    def pr_header(self):
+        print('| Package name | Assignee | Pull Request |')
+        print('|----|----|----|')
+
+    def pr_body(self, res):
+        print(f'| {res.name} | | |')
+        for p in res:
+            assignees = ", ".join([a.login for a in p.assignees]) or ''
+            print(f'| | {assignees} | #{p.number}: {p.title} |')
+
+    def pr_footer(self):
         pass
 
 
 class TextFormatter:
-    def header(self):
+    def commit_header(self):
         pass
 
-    def body_item(self, res):
+    def commit_body(self, res):
         print(f'- {res.name} (v{res.pypi_version})')
         for _dummy, headline in res:
             print(f'  - {headline}')
 
-    def footer(self):
+    def commit_footer(self):
+        pass
+
+    def pr_header(self):
+        pass
+
+    def pr_body(self, res):
+        print(f'- {res.name}')
+        for p in res:
+            assignees = ", ".join([a.login for a in p.assignees]) or 'UNASSIGNED'
+            print(f'  - #{p.number}: {p.title} ({assignees})')
+
+    def pr_footer(self):
         pass
 
 
@@ -60,13 +85,20 @@ class Comparison:
         self.pypi_version = pypi_version
         self.commits = commits
 
-    def has_commits(self):
-        return len(self.commits.commits) > 0
-
     def __iter__(self):
         for c in self.commits.commits:
             headline = c.message.split('\n')[0]
             yield c, headline
+
+
+class PRs:
+    def __init__(self, name, pulls):
+        self.name = name
+        self.pulls = pulls
+
+    def __iter__(self):
+        for p in self.pulls:
+            yield p
 
 
 def retrieve_info(package_name, client):
@@ -90,6 +122,19 @@ def compare_pkg(name, gh, client):
         repo.compare_commits(f'v{pypi_version}', 'master'),
     )
 
+
+def list_prs(name, gh):
+    repo = gh.repository('inveniosoftware', name)
+
+    return PRs(
+        name,
+        repo.pull_requests(state='open', sort='updated'),
+    )
+
+
+#
+# Click commands
+#
 @click.group()
 def cli():
     """GitHub inspection
@@ -122,10 +167,37 @@ def unreleased(packages, format, token):
         packages = GLOBAL_PACKAGES
 
     # Compare and output
-    formatter.header()
+    formatter.commit_header()
     for p in sorted(packages):
-        formatter.body_item(compare_pkg(p, gh, client))
-    formatter.footer()
+        formatter.commit_body(compare_pkg(p, gh, client))
+    formatter.commit_footer()
+
+
+
+@cli.command()
+@click.argument('packages', nargs=-1)
+@click.option('--format', '-f', type=click.Choice(['md', 'txt']), default='md', help="Output format.")
+@click.option('--token', '-t', required=True, help="GitHub access token.")
+def prs(packages, format, token):
+    """Show unreleased commits.
+
+    Examples:
+
+      python github.py prs -t <token> -f txt
+
+      python github.py unreleased -t <token> -f md invenio-records-resources
+    """
+    # Defaults
+    gh = login(token=token)
+    formatter = FormatterFactory.create(format)
+    if not packages:
+        packages = GLOBAL_PACKAGES
+
+    # Compare and output
+    formatter.pr_header()
+    for p in sorted(packages):
+        formatter.pr_body(list_prs(p, gh))
+    formatter.pr_footer()
 
 if __name__ == '__main__':
     cli()
